c79c956a5197d6ef80c132dceb5fafced6cfd9e5
Adrilr14 <adrilaro99@gmail.com>
2022-12-22 21:10:43 +0100

    Portfolio section updated, 1.0 Portfolio's version completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -201,10 +201,10 @@
   new Swiper('.portfolio-details-slider', {
     speed: 400,
     loop: true,
-    autoplay: {
+    autoplay: false/* {
       delay: 5000,
       disableOnInteraction: false
-    },
+    } */,
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
